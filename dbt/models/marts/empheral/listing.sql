c6ab0646c23390e71d3867526566026387eb722e
Sumeet karmankar <sumeetkarmankar42@gmail.com>
2026-02-15 12:10:03 +0000

    remove the duplicate dimensions

--- dbt/models/marts/empheral/listing.sql
+++ dbt/models/marts/empheral/listing.sql
@@ -2,15 +2,30 @@
 {{ config(materialized='ephemeral') }}
 
 with listings as (
-select 
-LISTING_ID,
+select LISTING_ID,
 PROPERTY_TYPE,
 ROOM_TYPE,
 CITY,
 COUNTRY,
 CAPACITY_CATEGORY,
 PRICE_CATEGORY,
-LISTING_CREATED_AT
- from 
-{{ ref('OBT') }}
+LISTING_CREATED_AT  from
+ ( select LISTING_ID,
+PROPERTY_TYPE,
+ROOM_TYPE,
+CITY,
+COUNTRY,
+CAPACITY_CATEGORY,
+PRICE_CATEGORY,
+LISTING_CREATED_AT , row_number() over (
+partition by LISTING_ID,
+PROPERTY_TYPE,
+ROOM_TYPE,
+CITY,
+COUNTRY,
+CAPACITY_CATEGORY,
+PRICE_CATEGORY,
+LISTING_CREATED_AT order by LISTING_ID 
+) rk from {{ ref('OBT') }}
+) where  rk = '1'
 ) select * from listings 
